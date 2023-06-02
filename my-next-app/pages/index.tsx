15367ac0a98743f7a5eea70d987cd919b8733095
Vitor Gerhardt <jvgerhardt@gmail.com>
2023-06-02 15:03:57 -0300

    initial commit

---
+++ my-next-app/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const Home: React.FC = () => {
+  return <div className={styles.container}>Hello, Next.js with TypeScript and SCSS!</div>;
+};
+
+export default Home;
