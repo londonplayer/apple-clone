311125159c39e7297cf9432f38f26c78c804ea9a
Vitor Gerhardt <jvgerhardt@gmail.com>
2023-06-05 13:47:16 -0300

    initial commit

---
+++ my-next-app/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './home.module.scss';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <h1>F</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- my-next-app/pages/index.tsx
+++ my-next-app/pages/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './index.module.scss';
+import HomePage from './home/home';
 
 const Home: React.FC = () => {
-  return <div className={styles.container}>Hello, Next.js with TypeScript and SCSS!</div>;
+  return (
+    <div className={styles.container}>
+      <HomePage />
+    </div>
+  );
 };
 
 export default Home;
